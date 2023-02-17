d3b55647fbf06d34ec062975e2f7ff3765e9f942
Blessed Tasela <taselablessed@gmail.com>
2023-02-17 22:59:53 +0000

    printing last number of random integer with %

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,31 +1,35 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* stdio for input and output function */
+
+/*
+* stdio for input and output function
+* Main - Entry Point
+*/
 
 /**
-* main: checks value of last random number using modulo (%)
-* conditions: if, else-if, else statements used
+* main - checks value of last random number using modulo (%)
+* description: checks value of random number
 * Return: always retuns 0 on success
 */
 int main(void)
 {
-	int n;
+	int n, digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int last_digit = n % 10;
+	digit = n % 10;
 
-	if (last_digit > 5)
+	if (digit > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 	}
-	else if (last_digit == 0)
+	else if (digit == 0)
 	{
-	printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	printf("Last digit of %d is %d and is 0\n", n, digit);
 	}
 	else
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	return (0);
 }
