77e0ce3880306f6d9439a5e8d8b95299e5b79e19
Blessed Tasela <taselablessed@gmail.com>
2023-02-17 02:25:56 +0000

    printing sizes of data types in c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+* stdio libary for input and output
+*
+* string library for char and puts function
+*
+* main - Entry point
+*
+*/
+
+/**
+* main - printf: prints the value of a string of characters
+*
+* Return: the value of printf
+*/
+int main(void)
+{
+	char a;
+	int b = sizeof(b);
+	long c = sizeof(c);
+	long long d = sizeof(d);
+	float e;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", b);
+	printf("Size of a long: %ld byte(s)\n", c);
+	printf("Size of a long long: %lld byte(s)\n", d);
+	printf("Size of a float: %zu byte (s)\n", sizeof(e));
+	return (0);
+}
