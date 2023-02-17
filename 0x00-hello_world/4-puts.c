83ebadd383281c197d1d90667bd04f0ad2cf00ba
Blessed Tasela <taselablessed@gmail.com>
2023-02-17 00:46:32 +0000

    printing output with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,17 @@
-#!/bin/bash
-gcc $CFILE
+#include <stdio.h>
+#include <string.h>
+
+/*
+*main - Entry point
+*
+*Return: always 0 (sucess)
+*
+*/
+int main(void)
+{
+	char string[100];
+
+	strcpy(string, "\"Programming is like building a multilingual puzzle");
+	puts(string);
+	return (0);
+}
