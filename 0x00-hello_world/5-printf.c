50bcc5c7d016e95bc4c86d40a0908c295bb908d5
Blessed Tasela <taselablessed@gmail.com>
2023-02-17 01:50:59 +0000

    printing string with function printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+* stdio libary for input and output
+*
+* string library for char and puts function
+*
+* main - Entry point
+*
+*/
+
+/**
+* main - printf: prints the value of a string of characters
+*
+* Return: the value of printf
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
