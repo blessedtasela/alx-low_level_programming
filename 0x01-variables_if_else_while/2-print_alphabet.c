bcffb4f5a9fcda2da2a3e5fa2db75f47a34ff09d
Blessed Tasela <taselablessed@gmail.com>
2023-02-18 00:10:27 +0000

    printing alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+* stdio libary for input and output
+*
+* string library for char and puts function
+*
+* main - Entry point
+*
+*/
+
+/**
+* main - putchar: prints the alpabets in lower case
+*
+* description: uses tolower function
+*
+* Return: the value of strcpy
+*/
+int main(void)
+{
+	char i;
+	char line = '\n';
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	putchar(line);
+	return (0);
+}
