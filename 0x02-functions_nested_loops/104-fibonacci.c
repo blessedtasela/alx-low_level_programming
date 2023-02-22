791e97bf89a3898dc0d6b0adab486dbce388a1ff
Blessed Tasela <taselablessed@gmail.com>
2023-02-22 04:10:32 +0000

    printing fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+* stdio libary for input and output
+*
+* string library for char and puts function
+*
+* main - Entry point
+*
+*/
+
+/**
+* main - print numbers of fibonnaci of 98 starting with 1 and 2
+* description: uses for loop and if statement
+* Return: return 0 on success
+*/
+int main(void)
+{
+	int i, num1 = 0, num2 = 1, fb, fdigit;
+
+		for (i = 1 ; i <= 98 ; i++)
+		{
+		fb = num1 + num2;
+		num1 = num2;
+		num2 = fb;
+		fdigit = fb;
+		while (fdigit >= 10)
+		{
+		fdigit = fdigit / 10;
+		}
+		if (fdigit == 1 || fdigit == 2)
+		printf("%d, ", fb);
+		}
+		printf("\n");
+		return (0);
+}
