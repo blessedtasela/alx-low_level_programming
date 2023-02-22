f7170a58ab62fe230cbc6e2580b52e054ea95901
Blessed Tasela <taselablessed@gmail.com>
2023-02-22 01:43:54 +0000

    printing hours of time in 24h format

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -22,15 +22,12 @@ void jack_bauer(void)
 	{
 		for (j = 0 ; j < 60 ; j++)
 		{
-		if (i < j)
-		{
 		_putchar((i / 10) + '0');
 		_putchar((i % 10) + '0');
 		_putchar(':');
 		_putchar((j / 10) + '0');
 		_putchar((j % 10) + '0');
 		_putchar('\n');
-		}
 		}
 	}
 }
