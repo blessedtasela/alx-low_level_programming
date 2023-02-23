6a947f13277ff859f9295c1a11b9f7439b66bec0
Blessed Tasela <taselablessed@gmail.com>
2023-02-23 01:03:02 +0000

    print first fibonacci numbers of 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,20 +11,23 @@
 */
 
 /**
-* main - print numbers of fibonnaci of 98 starting with 1 and 2
+* main - print sum of multiples 3 and 5
 * description: uses for loop and if statement
 * Return: return 0 on success
 */
 int main(void)
 {
-	int i, num1 = 0, num2 = 1, fb;
+	int i;
+	long int num1 = 0, num2 = 1, fb, sum = 0;
 
 		for (i = 1 ; i <= 98 ; i++)
 		{
 		fb = num1 + num2;
 		num1 = num2;
 		num2 = fb;
-		printf("%d, ", fb);
+		printf("%lu", fb);
+		if (i < 98)
+		printf(", ");
 		}
 		printf("\n");
 		return (0);
