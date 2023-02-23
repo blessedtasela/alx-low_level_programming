85ba45ef5ecda5564c57778621288cf5ce48c36d
Blessed Tasela <taselablessed@gmail.com>
2023-02-23 00:48:10 +0000

    printing fibomacci numbers of 50

---
+++ 0x02-functions_nested_loops/102-fibonaci.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+* stdio libary for input and output
+*
+* string library for char and puts function
+*
+* main - Entry point
+*
+*/
+
+/**
+* main - print fibonnaci number of 50
+* description: uses for loop and if statement
+* Return: return 0 on success
+*/
+int main(void)
+{
+	int i, num1 = 0, num2 = 1, fb;
+
+		for (i = 1 ; i <= 50 ; i++)
+		{
+		fb = num1 + num2;
+		num1 = num2;
+		num2 = fb;
+		printf("%d", fb);
+		if (i < 50)
+		printf(", ");
+		}
+		printf("\n");
+		return (0);
+}
