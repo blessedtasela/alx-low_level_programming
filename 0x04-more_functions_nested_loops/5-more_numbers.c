faa91176cebd32750d53e34f50460bbc49a510da
Blessed Tasela <taselablessed@gmail.com>
2023-02-24 17:14:36 +0000

    printing more numbers with _putchar function

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,8 +18,8 @@ void more_numbers(void)
 	for (i = 0 ; i < 15 ; i++)
 	{
 	if (i > 9)
-	_putchar((i / 10) + '0');
-	_putchar((i % 10) + '0');
+	_putchar((i / 10) + 48);
+	_putchar((i % 10) + 48);
 	}
 	_putchar('\n');
 	}
