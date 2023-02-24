93a1572cd1bd862b5d4f1023fa45f3fe1a86bd7e
Blessed Tasela <taselablessed@gmail.com>
2023-02-24 02:53:45 +0000

    printing numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,31 +7,15 @@
 */
 void print_number(int n)
 {
-		if (n / 10 == 0 && n == 0)
-		_putchar(n + '0');
-		else if (n / 10 == 0)
-		{
-		_putchar((n % 10) + '0');
-		}
-		else if (n / 100 == 0)
-		{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-		}
-		else if (n >= 100)
-		{
-		_putchar('0' + n / 100);
-		_putchar('0' + (n - 100) / 10);
-		_putchar('0' + n % 10);
-		}
-		else if (n >= 10000)
-		{
-		_putchar('0' + n / 100);
-		_putchar('0' + (n - 100) / 10);
-		_putchar('0' + n % 10);
-		}
-		else if (n / 10 <= 0)
-		_putchar('D');
-		else
-		_putchar('D');
+		unsigned int a;
+	
+	a = n;
+	if (n < 0)
+	{
+	_putchar(45);
+	a = -n;
+	}
+	if (a / 10)
+	print_number(a /10);
+	_putchar((a % 10) + '0');
 }
