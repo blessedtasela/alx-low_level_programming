15a26e474b15b12892f5d26c388047a41ed560dd
Blessed Tasela <taselablessed@gmail.com>
2023-02-24 16:00:02 +0000

    printing more numbers with _putchar function

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,18 +11,19 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, x;
 
 	for (j = 0 ; j < 11 ; j++)
 	{
 	for (i = 0 ; i < 15 ; i++)
 	{
-	_putchar((i % 10) + '0');
-	if (i / 10 == 0)
+	x = i;
+	if (i > 9)
 	{
-	continue;
+	_putchar('1');
+	x = i % 10;
 	}
-	_putchar((i / 10) + '0');
+	_putchar(x + '0');
 	}
 	_putchar('\n');
 	}
