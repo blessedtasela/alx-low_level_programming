7661f3c8586c2becef1deec91eb82215bf2d55af
Blessed Tasela <taselablessed@gmail.com>
2023-02-24 12:06:11 +0000

    printing diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,16 +14,21 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n != 0 || n > 0)
+	{
 	for (i = 0 ; i < n ; i++)
 	{
-	for (j = 0 ; j < n ; j++)
+	for (j = 0 ; j < i ; j++)
 	{
-	i = i + 1;
+	_putchar(' ');
+	}
 	_putchar('\\');
+	if (i == (n - 1))
+	continue;
+	_putchar('\n');
 	}
 	_putchar('\n');
-	_putchar(' ');
-	j = j - 1;
 	}
+	else
 	_putchar('\n');
 }
