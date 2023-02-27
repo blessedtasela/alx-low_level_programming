92c54e03c8beba0ac3de87c5265181689ec49b7d
Blessed Tasela <taselablessed@gmail.com>
2023-02-27 23:39:08 +0000

    printing array of pointer

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,7 +6,7 @@
  * Return: return arrayof char
  */
 
-int _puts(char *str)
+void _puts(char *str)
 {
 	while (*str != '\0')
 	{
@@ -14,5 +14,4 @@ int _puts(char *str)
 	str++;
 	}
 	_putchar('\n');
-	return (0);
 }
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - returns array of string in reverse order
+ * @s: variable to be used
+ * Return: return arrayof char
+ */
+
+int print_rev(char *s)
+{
+	while (*s > '\0')
+	{
+	_putchar(*str);
+	str--;
+	}
+	_putchar('\n');
+	return (0);
+}
