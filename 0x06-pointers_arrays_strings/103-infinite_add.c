740080a3a4f011dd7244933a728fcc370bc1f57e
Blessed Tasela <taselablessed@gmail.com>
2023-03-02 22:26:34 +0000

    adding two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,27 @@
 #include "main.h"
+/**
+ * rev_string - returns array of string in reverse order
+ * @s: variable to be used
+ * Return: return arrayof char
+ */
+
+void rev_string(char *s)
+{
+        int i, count = 0;
+        char rev = s[0];
+
+        while (s[count] != '\0')
+        {
+        count++;
+        }
+        for (i = 0 ; i < count ; i++)
+        {
+        count--;
+        rev = s[i];
+        s[i] = s[count];
+        s[count] = rev;
+        }
+}
 /*
 * main.h for input/output
 */
@@ -52,6 +75,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (num == size_r)
 	return (0);
 	*(r + num) = '\0';
-	
+	rev_string(r);
 	return (r);
 }
