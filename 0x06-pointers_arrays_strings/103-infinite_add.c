2a795bc4a7bc49dd33a00d8a7943ca31312bf5fd
Blessed Tasela <taselablessed@gmail.com>
2023-03-02 22:28:22 +0000

    adding two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -7,20 +7,20 @@
 
 void rev_string(char *s)
 {
-        int i, count = 0;
-        char rev = s[0];
+	int i, count = 0;
+	char rev = s[0];
 
-        while (s[count] != '\0')
-        {
-        count++;
-        }
-        for (i = 0 ; i < count ; i++)
-        {
-        count--;
-        rev = s[i];
-        s[i] = s[count];
-        s[count] = rev;
-        }
+	while (s[count] != '\0')
+	{
+	count++;
+	}
+	for (i = 0 ; i < count ; i++)
+	{
+	count--;
+	rev = s[i];
+	s[i] = s[count];
+	s[count] = rev;
+	}
 }
 /*
 * main.h for input/output
