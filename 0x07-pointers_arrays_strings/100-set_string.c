109126bc9b228a1ba810e5f92842f6b17117d913
Blessed Tasela <taselablessed@gmail.com>
2023-03-06 22:59:31 +0000

    pointer to pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/*
+* main.h for input/output
+*/
+
+/**
+* set_strings- function that points a pointer to a pointer
+* @s: first pointer
+* @to: second pointer
+* Return: returns 0 on success
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -17,7 +17,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
-void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 int reverse_string(char *s);
 void print_buffer(char *b, int size);
