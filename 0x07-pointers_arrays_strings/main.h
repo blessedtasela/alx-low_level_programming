a93b623fa9a89e6b6c5742ce1e9d1c4a29cbcd36
Blessed Tasela <taselablessed@gmail.com>
2023-03-06 21:15:18 +0000

    function that searches for character in string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,7 +10,7 @@
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strncpy(char *dest, char *src, int n);
+char *_strchr(char *s, char c);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *n);
