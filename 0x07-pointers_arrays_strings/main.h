62bebe8b8f4558dec6a4cd31ee373a10d4258708
Blessed Tasela <taselablessed@gmail.com>
2023-03-06 21:53:29 +0000

    function that searches a string for set of bytes

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,7 +12,7 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-void reverse_array(int *a, int n);
+char *_strpbrk(char *s, char *accept);
 char *string_toupper(char *n);
 char *cap_string(char *n);
 char *leet(char *n);
