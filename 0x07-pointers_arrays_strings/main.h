1bcb0d96398bfe4750ad048b8cfaf44e82d15564
Blessed Tasela <taselablessed@gmail.com>
2023-03-06 21:36:45 +0000

    function that gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,7 +11,7 @@
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-int _strcmp(char *s1, char *s2);
+unsigned int _strspn(char *s, char *accept);
 void reverse_array(int *a, int n);
 char *string_toupper(char *n);
 char *cap_string(char *n);
