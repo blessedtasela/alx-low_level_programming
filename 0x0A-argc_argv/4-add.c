5b952da6badb52679d0b62df4593410c7317e481
Blessed Tasela <taselablessed@gmail.com>
2023-03-12 22:35:12 +0000

    adding multiple numbers from parameter

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - Entry point to add numbers
+ * @argc: arguement counter
+ * @argv: arguement pointer to string
+ * Return: 0 0n success
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+unsigned int j, sum = 0;
+char *c;
+
+if (argc == 1)
+printf("0\n");
+else
+{
+for (i = 1 ; i < argc ; i++)
+{
+c = argv[i];
+for (j = 0 ; j < strlen(c) ; j++)
+{
+if (c[j] < 48 || c[j] > 57)
+{
+printf("Error\n");
+return (1);
+}
+else
+sum += atoi(c);
+c++;
+}
+}
+printf("%d\n", sum);
+}
+return (0);
+}
