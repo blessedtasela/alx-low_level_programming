ec0c6f4b8405bb4d08d0ff24831935d2a124153e
Blessed Tasela <taselablessed@gmail.com>
2023-03-12 22:44:47 +0000

    adding multiple numbers from parameter

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+* description: declares prototype for function declaration
+*/
+/**
+* _putchar: function prototype to print character
+*/
+
+int _putchar(char i);
+
+#endif
