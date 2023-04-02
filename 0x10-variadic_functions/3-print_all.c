7b5dede51ea9da3f39a6e2c04a0009c5c084af8e
Blessed Tasela <taselablessed@gmail.com>
2023-04-02 05:21:18 +0000

    printing all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,35 +10,42 @@
 
 void print_all(const char * const format, ...)
 {
-	char *str, *separator = "";
+	char *str, *sep = "";
+	int i = 0;
+
 	va_list list;
-	int i;
 
 	va_start(list, format);
 
-	i = 0;
+	if (format)
+	{
 		while (format[i])
 		{
 		switch (format[i])
 		{
 			case 'c':
-			printf("%s%c", separator, va_arg(list, int));
+			printf("%s%c", sep, va_arg(list, int));
+			break;
 			case 'i':
-			printf("%s%d", separator, va_arg(list, int));
+			printf("%s%d", sep, va_arg(list, int));
+			break;
 			case 'f':
-			printf("%s%f", separator, va_arg(list, double));
+			printf("%s%f", sep, va_arg(list, double));
+			break;
 			case 's':
 			str = va_arg(list, char *);
-			if (str == NULL)
-			printf("(nil)");
-			printf("%s%s", separator, str);
+			if (!str)
+			str = "(nil)";
+			printf("%s%s", sep, str);
 			break;
 			default:
 			i++;
+			continue;
 		}
-		separator = ", ";
+		sep = ", ";
 		i++;
 		}
-	printf("\n");
+		}
 	va_end(list);
+	printf("\n");
 }
