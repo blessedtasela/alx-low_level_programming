ec387613be28372e5e7433a1394dd344db6208aa
Blessed Tasela <taselablessed@gmail.com>
2023-04-06 00:44:29 +0000

    inserting new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,9 +22,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
-	new->n = n;
-	new->next = NULL;
-
 	if (idx == 0)
 	{
 	new->next = *head;
@@ -34,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (i = 0; temp && i < idx; i++)
 	{
-	if (idx - 1)
+	if (i == idx - 1)
 	{
 	new->next = temp->next;
 	temp->next = new;
